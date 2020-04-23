aa74c181e708e33d89979b74c15b31d3f4fd2e23
Maria <mar1asm@yahoo.com>
2020-04-23 20:01:33 +0300

    com

--- src/app/components/battle/battle.component.ts
+++ src/app/components/battle/battle.component.ts
@@ -8,7 +8,8 @@ import { Router } from '@angular/router';
 })
 export class BattleComponent implements OnInit {
 
-  _username='';
+  _username1='';
+  _username2='';
   cauta=false;
   constructor(private router: Router
   ) { }
@@ -18,14 +19,15 @@ export class BattleComponent implements OnInit {
   }
 
 
-  onBattle(value:string):void{
-    this._username=value;
-    console.log(this._username);
-    if (this._username)
+  onBattle(username1:string, username2:string):void{
+    this._username1=username1;
+    this._username2=username2;
+    console.log(this._username1);
+    if (this._username1 && this._username2)
       {
         this.router.navigate(['/profiles'],
         {
-          queryParams:{p1: this._username, p2: this._username}
+          queryParams:{p1: this._username1, p2: this._username2}
         });
         
       } else
