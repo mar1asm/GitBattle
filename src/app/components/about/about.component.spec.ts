97ac923096af96b0eee25631e2298bb8fdcb47c8
Maria <mar1asm@yahoo.com>
2020-04-26 00:54:21 +0300

    up

--- src/app/components/about/about.component.spec.ts
+++ src/app/components/about/about.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AboutGithubComponent } from './about-github.component';
+import { AboutComponent } from './about.component';
 
-describe('AboutGithubComponent', () => {
-  let component: AboutGithubComponent;
-  let fixture: ComponentFixture<AboutGithubComponent>;
+describe('AboutComponent', () => {
+  let component: AboutComponent;
+  let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AboutGithubComponent ]
+      declarations: [ AboutComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(AboutGithubComponent);
+    fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -15,14 +15,16 @@ import * as battleActions from '../battle/state/battle.actions';
 
 export class ProfileComponent implements OnInit {
 
-  firstProfileName = '';
-  secondProfileName = '';
-
-  firstProfileScore = 0;
-  secondProfileScore = 0;
-
-  firstProfile: IProfile;
-  secondProfile: IProfile;
+  firstProfile={
+    profile : <IProfile> null,
+    name: <string> null,
+    score: <number>0
+  }
+  secondProfile={
+    profile : <IProfile> null,
+    name: <string> null,
+    score: <number>0
+  }
 
   private loading = false;
 
@@ -32,19 +34,19 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.secondProfileName = this.route.snapshot.queryParamMap.get('p2');
-    this.firstProfileName = this.route.snapshot.queryParamMap.get('p1');
+    this.firstProfile.name = this.route.snapshot.queryParamMap.get('p1');
+    this.secondProfile.name = this.route.snapshot.queryParamMap.get('p2');
     this.doSearch();
   }
 
   doSearch() {
     this.loading = true;  //add some loading animation
-    this.profileService.searchProfile(this.firstProfileName).then(() => {
-      this.firstProfile = this.profileService.getProfileData();
-      this.profileService.searchProfile(this.secondProfileName).then(() => {
-        this.secondProfile = this.profileService.getProfileData();
+    this.profileService.searchProfile(this.firstProfile.name).then(() => {
+      this.firstProfile.profile = this.profileService.getProfileData();
+      this.profileService.searchProfile(this.secondProfile.name).then(() => {
+        this.secondProfile.profile = this.profileService.getProfileData();
         this.loading = false;
-        if (this.firstProfile && this.secondProfile) {
+        if (this.firstProfile.profile && this.secondProfile.profile) {
           this.getComparisonOptions();
         } else {
           console.log('error');
@@ -57,8 +59,6 @@ export class ProfileComponent implements OnInit {
     this.store.pipe(select(fromBattle.getBattleState)).subscribe(
       compareBy => {
         this.calculateScore(compareBy);
-        console.log(this.firstProfileScore);
-        console.log(this.secondProfileScore);
       }
     );
   }
@@ -67,35 +67,35 @@ export class ProfileComponent implements OnInit {
 
   calculateScore(compareBy: fromBattle.BattleState): void {
     if (compareBy.compareByRepos) {
-      this.firstProfileScore += this.firstProfile.public_repos;
-      this.secondProfileScore += this.secondProfile.public_repos;
+      this.firstProfile.score += this.firstProfile.profile.public_repos;
+      this.secondProfile.score += this.secondProfile.profile.public_repos;
     }
 
     if (compareBy.compareByGists) {
-      this.firstProfileScore += this.firstProfile.public_gists;
-      this.secondProfileScore += this.secondProfile.public_gists;
+      this.firstProfile.score += this.firstProfile.profile.public_gists;
+      this.secondProfile.score += this.secondProfile.profile.public_gists;
     }
     if (compareBy.compareByFollowers) {
-      this.firstProfileScore += this.firstProfile.followers;
-      this.secondProfileScore += this.secondProfile.followers;
+      this.firstProfile.score += this.firstProfile.profile.followers;
+      this.secondProfile.score += this.secondProfile.profile.followers;
     }
     if (compareBy.compareByBlog) {
-      if (this.firstProfile.blog)
-        this.firstProfileScore += 10;
-      if (this.secondProfile.blog)
-        this.secondProfileScore += 10;
+      if (this.firstProfile.profile.blog)
+        this.firstProfile.score += 10;
+      if (this.secondProfile.profile.blog)
+        this.secondProfile.score += 10;
     }
     if (compareBy.compareByCompany) {
-      if (this.firstProfile.company)
-        this.firstProfileScore += 100;
-      if (this.secondProfile.company)
-        this.secondProfileScore += 100;
+      if (this.firstProfile.profile.company)
+        this.firstProfile.score += 100;
+      if (this.secondProfile.profile.company)
+        this.secondProfile.score += 100;
     }
     if (compareBy.compareByVechime) {
-      if (this.firstProfile.created_at > this.firstProfile.created_at)
-        this.firstProfileScore += 10; else
-        if (this.firstProfile.created_at < this.firstProfile.created_at)
-          this.secondProfileScore += 10;
+      if (this.firstProfile.profile.created_at > this.firstProfile.profile.created_at)
+        this.firstProfile.score += 10; else
+        if (this.firstProfile.profile.created_at < this.firstProfile.profile.created_at)
+          this.secondProfile.score += 10;
     }
   }
 
